d324513d24bff9c706f8e1802adf253253935d75
Edilson Rodrigues <edarodrigs@gmail.com>
2021-01-18 11:52:52 -0300

    componente com propriedade

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import ReactDom from 'react-dom'
 import React from 'react'
 
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 ReactDom.render(
   <div>
     <Primeiro></Primeiro>
+    <ComParametro
+      titulo="Situação do Aluno"
+      aluno="Pedro"
+      nota={9.3}
+    />
   </div>,
   document.getElementById('root'),
 );
