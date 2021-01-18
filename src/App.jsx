42d71fb01657cb7b2ca732a0dc826ed6a13fa30b
Edilson Rodrigues <edarodrigs@gmail.com>
2021-01-18 14:34:50 -0300

    componente card #01

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,32 @@ import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
 import Aleatorio from './components/basicos/Aleatorio';
+import Card from './components/layout/Card'
 
 // eslint-disable-next-line
 export default () =>
     <div id="app">
         <h1>Fundamentos React</h1>
-        <Aleatorio min={0} max={60} />
-        <Fragmento />
-        <ComParametro
-            titulo="Situação do Aluno"
-            aluno="Pedro"
-            nota={9.3}
-        />
-        <Primeiro></Primeiro>
+        <Card titulo="#04 - Desafio Aleatório">
+            <Aleatorio min={0} max={60} />
+        </Card>
+
+        <Card titulo="#03 - Fragmento">
+            <Fragmento />
+        </Card>
+
+        <Card titulo="#02 - Com Parâmetro">
+            <ComParametro
+                titulo="Situação do Aluno"
+                aluno="Pedro"
+                nota={9.3}
+            />
+        </Card>
+        <Card titulo="#01 - Primeiro">
+            <Primeiro></Primeiro>
+        </Card>
+
+
+
     </div>
 
