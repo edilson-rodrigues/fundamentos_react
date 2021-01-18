e64eda4971f42ccd34b920f6b03902ee2502ad6d
Edilson Rodrigues <edarodrigs@gmail.com>
2021-01-18 17:38:52 -0300

    rendirização condicional #01

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 
+import ParOuImpar from './components/condicional/ParOuImpar'
 import TabelaProdutos from './components/repeticao/TabelaProdutos';
 import ListaAlunos from './components/repeticao/ListaAlunos';
 import Primeiro from './components/basicos/Primeiro';
@@ -18,7 +19,16 @@ export default () =>
 
         <h1>Fundamentos React</h1>
 
+        {/* background-image: linear-gradient(-225deg, #22E1FF 0%, #1D8FE1 48%, #625EB1 100%); */}
+
         <div className="Cards">
+            <Card
+                titulo="#08 - Renderização Condicional"
+                firstColor="#22E1FF"
+                secoundColor="#1D8FE1"
+            >
+                <ParOuImpar numero={20} />
+            </Card>
             <Card
                 titulo="#07 - Desafio Repetição"
                 firstColor="#B6CEE8"
