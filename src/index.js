0607d88f6e42a4d36a73315a3ce5f8ba960548ab
Edilson Rodrigues <edarodrigs@gmail.com>
2021-01-18 11:21:01 -0300

    conhecendo jsx

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import ReactDom from 'react-dom'
+import React from 'react'
+
+
+const tag = <strong>Olá React!!!</strong>;
+
+
+ReactDom.render(
+  <div>
+    {tag}
+  </div>,
+  document.getElementById('root'),
+);
