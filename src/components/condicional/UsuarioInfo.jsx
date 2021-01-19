84d3c95fdbe14bd324a83ad0b362ce75e3b72ca8
Edilson Rodrigues <edarodrigs@gmail.com>
2021-01-19 08:51:19 -0300

    renderização condicional #03

--- src/components/condicional/UsuarioInfo.jsx
+++ src/components/condicional/UsuarioInfo.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import If from './if'
+import If, { Else } from './if';
 
 // eslint-disable-next-line
 export default props => {
     const usuario = props.usuario || {}
     return (
         <div>
-            <If test={usuario && usuario.nome}>
+            {/*  <If test={usuario && usuario.nome}>
                 <p>Seja Bem Vindo <strong>{usuario.nome}</strong> !</p>
             </If>
-            <If test={!usuario || usuario.nome}>
+            <If test={!usuario || !usuario.nome}>
                 <p>Seja Bem Vindo <strong>Visitante</strong> !</p>
+            </If> */}
+            <If test={usuario && usuario.nome}>
+                Seja Bem vindo <strong>{usuario.nome}</strong>!
+                <Else>
+                    Seja Bem vindo <strong>Visitante</strong>!
+                </Else>
             </If>
         </div>
     )
